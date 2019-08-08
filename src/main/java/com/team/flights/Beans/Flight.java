fec4617d05587c0b9f4d0c0bdad95a22faec56d7
Justin <justinyang1000@hotmail.com>
2019-08-08 11:43:03 -0400

    Initial Classes and Repositories

--- src/main/java/com/team/flights/Beans/Flight.java
+++ src/main/java/com/team/flights/Beans/Flight.java
@@ -36,4 +36,84 @@ public class Flight {
 
     @Column(name = "trip_id")
     private long tripId;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getToLocation() {
+        return toLocation;
+    }
+
+    public void setToLocation(String toLocation) {
+        this.toLocation = toLocation;
+    }
+
+    public long getFromLocation() {
+        return fromLocation;
+    }
+
+    public void setFromLocation(long fromLocation) {
+        this.fromLocation = fromLocation;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDepartingTime() {
+        return departingTime;
+    }
+
+    public void setDepartingTime(String departingTime) {
+        this.departingTime = departingTime;
+    }
+
+    public String getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(String arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public long getFlightClass() {
+        return flightClass;
+    }
+
+    public void setFlightClass(long flightClass) {
+        this.flightClass = flightClass;
+    }
+
+    public long getAvaliableSeats() {
+        return avaliableSeats;
+    }
+
+    public void setAvaliableSeats(long avaliableSeats) {
+        this.avaliableSeats = avaliableSeats;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public long getTripId() {
+        return tripId;
+    }
+
+    public void setTripId(long tripId) {
+        this.tripId = tripId;
+    }
 }
