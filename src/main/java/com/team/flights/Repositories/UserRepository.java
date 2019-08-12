225cdff3599243e1bc038bd9d5e2382da09d39f5
Justin <justinyang1000@hotmail.com>
2019-08-12 16:15:26 -0400

    Admin Page fixed Update User function

--- src/main/java/com/team/flights/Repositories/UserRepository.java
+++ src/main/java/com/team/flights/Repositories/UserRepository.java
@@ -1,13 +1,55 @@
 package com.team.flights.Repositories;
 
 import com.team.flights.Beans.User;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
+import javax.transaction.Transactional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByUsername(String name);
+//    //------------------------------------------------------------------------------------------------------------------
+//    @Modifying
+//    @Transactional
+//    @Query("UPDATE User user SET user.firstName = :firstName WHERE user.id = :id")
+//    int updateFirstName(@Param("id") long id, @Param("firstName") String firstName);
+//    //------------------------------------------------------------------------------------------------------------------
+//    @Modifying
+//    @Transactional
+//    @Query("UPDATE User user SET user.lastName = :lastName WHERE user.id = :id")
+//    int updateLastName(@Param("id") long id, @Param("lastName") String lastName);
+//    //------------------------------------------------------------------------------------------------------------------
+//    @Modifying
+//    @Transactional
+//    @Query("UPDATE User user SET user.birthDate = :birthDate WHERE user.id = :id")
+//    int updateBirthDate(@Param("id") long id, @Param("birthDate") String birthDate);
+//    //------------------------------------------------------------------------------------------------------------------
+//    @Modifying
+//    @Transactional
+//    @Query("UPDATE User user SET user.country = :country WHERE user.id = :id")
+//    int updateCountry(@Param("id") long id, @Param("country") String country);
+//    //------------------------------------------------------------------------------------------------------------------
+//    @Modifying
+//    @Transactional
+//    @Query("UPDATE User user SET user.email = :email WHERE user.id = :id")
+//    int updateEmail(@Param("id") long id, @Param("email") String email);
+//    //------------------------------------------------------------------------------------------------------------------
+//    @Modifying
+//    @Transactional
+//    @Query("UPDATE User user SET user.phone = :phone WHERE user.id = :id")
+//    int updatePhone(@Param("id") long id, @Param("phone") String phone);
+//    //------------------------------------------------------------------------------------------------------------------
+//    @Modifying
+//    @Transactional
+//    @Query("UPDATE User user SET user.username = :username WHERE user.id = :id")
+//    int updateUsername(@Param("id") long id, @Param("username") String username);
+//    //------------------------------------------------------------------------------------------------------------------
+//    @Modifying
+//    @Transactional
+//    @Query("UPDATE User user SET user.password = :password WHERE user.id = :id")
+//    int updatePassword(@Param("id") long id, @Param("password") String password);
+//    //------------------------------------------------------------------------------------------------------------------
 
-    @Query("UPDATE User u SET u.address = :address WHERE u.id = :userId")
-    int updateAddress(@Param("userId") int userId, @Param("address") String address);
 }
